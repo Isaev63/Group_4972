f539b98819eadd8ed1095f2b75878506aded6268
Isaev_Anton <gsgmad@mail.ru>
2023-09-05 12:46:19 +0400

    Sem1Task4/Program.cs update

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+int numA = int.Parse(Console.ReadLine() ?? "0");
+int numB = int.Parse(Console.ReadLine() ?? "0");
+
+if (numA > numB)
+{
+    Console.WriteLine("Число " + numA + " больше числа " + numB);
+}
+else
+{
+    Console.WriteLine("Число " + numA + " меньше числа " + numB);
+}
---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+int numA = int.Parse(Console.ReadLine() ?? "0");
+int numB = int.Parse(Console.ReadLine() ?? "0");
+int numC = int.Parse(Console.ReadLine() ?? "0");
+int max = numA;
+
+if (numB > max) max = numB;
+if (numC > max) max = numC;
+
+Console.WriteLine("максимальное число " + max);
