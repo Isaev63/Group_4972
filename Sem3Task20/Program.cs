e9f489b085f0f44a7cefa749e9c21181e24a9ea7
Isaev_Anton <gsgmad@mail.ru>
2023-09-13 23:06:05 +0400

    solution from seminar 3

---
+++ Sem3Task20/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача №20
+// Напишите программу, 
+// которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 2D пространстве.
+
+// Вводим данные
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+// Определяем длинну
+double Culc2DDist(int x1, int x2, int y1, int y2)
+{
+    double res = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)); // 3d (z1 - z2)
+
+    return res;
+}
+
+// Печатаем результат
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+
+int x1 = ReadData("Введите координату x1: ");
+int x2 = ReadData("Введите координату x2: ");
+int y1 = ReadData("Введите координату y1: ");
+int y2 = ReadData("Введите координату y2: ");
+double result = Culc2DDist(x1, x2, y1, y2);
+PrintResult("Расстояние " + result);
---
+++ Sem3Task22/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача №22
+// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу квадратов чисел от 1 до N.
+
+// Вводим данные
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine() ?? "0");
+    return num;
+}
+
+// Определяем квадрат чисел
+string BuildLine(int num, int pow)
+{
+    string result = String.Empty;
+    for (int i = 1; i <= num; i++)
+    {
+        result = result + Math.Pow(i, pow) + ", ";
+    }
+    return result;
+}
+
+// Печатаем результат
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+int num = ReadData("Введите число: ");
+int pow = ReadData("Введите степень: ");
+string result = BuildLine(num, pow);
+PrintResult("Расстояние " + result);
